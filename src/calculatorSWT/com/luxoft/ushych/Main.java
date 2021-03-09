b51897abb7b58144f7f285cd0312ddf0eb6bfa7f
Valentyn Ushych <valentinushych@gmail.com>
2021-03-09 13:57:40 +0200

    create main

---
+++ src/calculatorSWT/com/luxoft/ushych/Main.java
@@ -0,0 +1,12 @@
+package calculatorSWT.com.luxoft.ushych;
+
+import calculatorSWT.com.luxoft.ushych.views.ViewController;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ViewController view = new ViewController();
+        view.show();
+    }
+}
